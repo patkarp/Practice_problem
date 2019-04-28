c1551067a287633260b7ab7f850353299af2bdaa
patkarp <abhi1991in@gmail.com>
2019-04-28 15:30:28 -0700

    added new programs

--- Java/Beginner/BubbleSortString.java
+++ Java/Beginner/BubbleSortString.java
@@ -3,10 +3,24 @@ import java.lang.*;
 import java.util.*;
 class BubbleSortString
 {
+	void sort(String citynames[], int cityarrsize){
+		for(int i= 0; i < cityarrsize;i++){
+			for(int j=0;j<cityarrsize-i-1;i++){
+				
+				
+			}
+		}
+	}
 	public static void main(String args[])
 	{
 		String citynames[]={"Chicago", "Paris", "NY", "Portland", "Mumbai", "SFO"};
-		int cityarrsize=citynames.lengthl
+		int cityarrsize=citynames.length;
+
+		BubbleSortString b = new BubbleSortString();
+		b.sort(citynames, cityarrsize);
+		Arrays.sort(citynames);
+
+
 
 	}
 }
